aff87fd208567222b6d9685d6b4ee90412d3cdff
ozwonek <azwonek@student.agh.edu.pl>
2025-01-04 20:07:30 +0100

    naprawa visualatora dodane do segment

--- code/visualisation.py
+++ code/visualisation.py
@@ -9,5 +9,27 @@ def show_map(Trapezoids : list[Trapezoid] , lines : list[Segment]):
     for line in lines:
         plt.plot([line.left.x, line.right.x], [ line.left.y, line.right.y], color = "blue")
     for trapez in Trapezoids:
-        plt.plot([trapez.top.left.x, trapez.bottom.left.x], [trapez.top.left.y, trapez.bottom.left.y], color = "green")
+        bottom = trapez.bottom
+        top = trapez.top
+        left = trapez.left
+        right = trapez.right
+        if bottom.point_over_segment(left) == 0:
+            A = left
+            B = top.value_for_x(left.x)
+        else:
+            A = bottom.value_for_y(left.x)
+            B = left
+        if bottom.point_over_segment(right) == 0:
+            C = right
+            D = top.value_for_x(right.x)
+        else:
+            C = top.value_for_x(right.x)
+            D = right
+        plt.plot([A.x, B.x], [A.y, B.y], color = "green")
+        plt.plot([C.x, D.x], [C.y, D.y], color = "green")
+
     plt.show()
+
+
+
+    
