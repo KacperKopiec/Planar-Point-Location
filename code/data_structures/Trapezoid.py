9c15e2f688b30ce89b08596460f1cf68b5af346a
ozwonek <azwonek@student.agh.edu.pl>
2025-01-04 20:54:55 +0100

    zmiany w trapezoidzie, segmencie oraz w wizualizacji

--- code/data_structures/Trapezoid.py
+++ code/data_structures/Trapezoid.py
@@ -7,6 +7,17 @@ class Trapezoid:
         self.right = right
         self.top = top
         self.bottom = bottom
+    
+    def trapezoidBoundary(self):
+        bottom = self.bottom
+        top = self.top
+        left = self.left
+        right = self.right
+        A = bottom.value_for_x(left.x)
+        B = top.value_for_x(left.x)
+        C = bottom.value_for_x(right.x)
+        D = top.value_for_x(right.x)
+        return A,B,C,D
 
     def __hash__(self):
         return hash((self.left, self.right))
